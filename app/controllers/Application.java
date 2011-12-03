0e9bad65b8ca76a0299f38c79e5abb2931f7ceb0
Kerner Dennis <denkern@gmail.com>
2011-12-03 17:19:54 +0800

    Find

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,8 +6,13 @@ import play.mvc.Scope.Session;
 
 import java.util.*;
 
+import net.spy.memcached.ops.ConcatenationType;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+
+import core.DataFinder;
+import core.DataFinder.ConditionTypes;
 
 import models.*;
 
@@ -42,5 +47,26 @@ public class Application extends Controller {
     	ClickStreamEvent e = new Gson().fromJson(body, ClickStreamEvent.class);        
     	return e; 
     }
+    
+    public static void Find(JsonObject obj)
+    {
+    
+    
+    	try {
+			DataFinder finder = new DataFinder(ClickStreamEvent.class).Where(obj.get("field").toString(), ConditionTypes.Equals, obj.get("fieldValue").toString());
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    
+    }
+    
 
 }
--- app/controllers/StreamEvents.java
+++ app/controllers/StreamEvents.java
@@ -3,11 +3,16 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.mvc.Scope.Session;
+import support.LogWriter;
 
 import java.util.*;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import core.DataFinder;
+import core.DataFinder.ConditionTypes;
 
 import models.*;
 
@@ -15,5 +20,85 @@ public class StreamEvents extends Controller {
 	
 	public static void index() {
 		render();
+	}
+	
+	public static void addEvent(JsonObject body) {
+	    	
+	    	ClickStreamEvent e = handleJsonAsObject(body);
+	    	if (e== null)
+	    	{
+	    		//render("Empty!!!");
+	    		render(body);
+	    		// add error message
+	    		return;
+	    	}
+	    	e.sessionId = Session.current().getId();
+	   
+	    	e.Save(); 
+	    } 
+	  
+	public static void handleJson(JsonObject body) 
+	{        
+	    	renderText(body.getAsJsonPrimitive("name").getAsString());    
+	}        
+	
+	public static ClickStreamEvent handleJsonAsObject(JsonObject body) 
+	{        
+		ClickStreamEvent e = new Gson().fromJson(body, ClickStreamEvent.class);        
+		return e; 
+	}
+	
+	
+	public static void Find(JsonObject obj)
+	{
+	
+		ArrayList<ClickStreamEvent> array = new ArrayList<ClickStreamEvent>();
+		
+		
+			DataFinder finder;
+			try 
+			{
+				
+				//finder = new DataFinder(ClickStreamEvent.class).Where(obj.get("field").toString(), ConditionTypes.Equals, obj.get("fieldValue").toString());
+				
+				finder = new DataFinder(ClickStreamEvent.class);
+				
+				boolean notEnd;
+				ClickStreamEvent event = null; 
+				JsonObject element = null;
+				LogWriter log = new LogWriter();
+				
+				while (true) 
+				{
+					event = new ClickStreamEvent();
+					event.Name = "Test1";
+					event.CreatedOn = new Date();
+					event.elementType = "Button";
+					array.add(event);
+					
+					
+					event = new ClickStreamEvent();
+					event.Name = "Test2";
+					event.CreatedOn = new Date();
+					event.elementType = "Div";
+					array.add(event);
+					
+					break;
+					
+					//event = (ClickStreamEvent) finder.Next();
+					//log.WriteToFile("Test", true);
+					//if (event == null)
+					//	break;
+					//array.add(event);
+				} 
+			
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		
+		
+		renderJSON(array);
+		
 	}
 }
