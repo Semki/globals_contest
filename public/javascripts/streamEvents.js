26972aa89755c84655bb58ebabbb87eed8fc61da
Anton Zhavoronkov <djzhan@list.ru>
2011-12-03 19:54:07 +0800

    time

--- public/javascripts/streamEvents.js
+++ public/javascripts/streamEvents.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
 	
-	$("#time2").timePicker({
-		  startTime: "02.00", // Using string. Can take string or Date object.
-		  endTime: new Date(0, 0, 0, 15, 30, 0), // Using Date object here.
-		  show24Hours: false,
-		  separator: '.',
-		  step: 15});
+
 	
-	
-	$( "#datepicker" ).datepicker();
+	//$.datepicker.setDefaults({ dateFormat: 'yy-mm-dd' });
+	$("#datepicker1").datetimepicker({dateFormat: 'yy-mm-dd'});
+	$("#datepicker2").datetimepicker({dateFormat: 'yy-mm-dd'});
 	
 	
 	requestData(null);
 	
 	$("#apply_filter").click(function () {
-		var filter = {"elementType": $('#element_type').val(), "elementId": $('#element_id').val(), "elementClass": $('#element_class').val()};
+		var filter = {"elementType": $('#element_type').val(), "elementId": $('#element_id').val(), "elementClass": $('#element_class').val(),
+				"created_at_start":$('#datepicker1').val(),
+				"creted_at_finish":$('#datepicker2').val()};
 		
 		requestData(filter);
 		
