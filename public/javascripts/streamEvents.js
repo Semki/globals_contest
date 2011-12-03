66f86a90a272b348c27f2e5bf581f276188f4904
Anton Zhavoronkov <djzhan@list.ru>
2011-12-03 18:48:17 +0800

    filter

--- public/javascripts/streamEvents.js
+++ public/javascripts/streamEvents.js
@@ -1,19 +1,41 @@
 $(document).ready(function () {
 	
+	$("#time2").timePicker({
+		  startTime: "02.00", // Using string. Can take string or Date object.
+		  endTime: new Date(0, 0, 0, 15, 30, 0), // Using Date object here.
+		  show24Hours: false,
+		  separator: '.',
+		  step: 15});
 	
-	$.get("streamevents/find", null,function (data) {
-		iterateData(data);
-	}, "json");
+	
+	$( "#datepicker" ).datepicker();
+	
+	
+	requestData(null);
+	
+	$("#apply_filter").click(function () {
+		var filter = {"elementType": $('#element_type').val(), "elementId": $('#element_id').val(), "elementClass": $('#element_class').val()};
+		
+		requestData(filter);
+		
+	});
 	
 });
 
+requestData = function (filter) {
+	$.post("streamevents/find", JSON.stringify(filter),function (data) {
+		iterateData(data);
+	}, "json");
+};
+
 
 iterateData = function (data) {
+	$('#events_table').html('');
 	for (var i in data)
 	{
 		$("#events_table").append('<tr></tr>');
 		$("#events_table tr:last-child").append('<td>' + data[i].Name + '</td');
-		$("#events_table tr:last-child").append('<td>' + data[i].elementType + '</td');
+		$("#events_table tr:last-child").append('<td>&lt;' + data[i].elementType + '&gt;</td');
 		$("#events_table tr:last-child").append('<td>' + data[i].CreatedOn + '</td');
 	}
 };
