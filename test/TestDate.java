66d16e9e7a8ecd79deb650222ddf73e055007681
Kerner Dennis <denkern@gmail.com>
2011-12-04 12:18:53 +0800

    DateWork

---
+++ test/TestDate.java
@@ -0,0 +1,15 @@
+import core.DateHelper;
+
+
+public class TestDate {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(DateHelper.DateToString(null));
+
+	}
+
+}
