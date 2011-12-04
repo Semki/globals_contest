d1f8fa95d4a08a430b8e39f4f12ca7898cd39f23
antoshalee <antoshalee@gmail.com>
2011-12-04 13:50:09 +0800

    click me application

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -22,51 +22,7 @@ public class Application extends Controller {
         render();
     }
     
-    public static void addEvent(JsonObject body) {
-    	
-    	ClickStreamEvent e = handleJsonAsObject(body);
-    	if (e== null)
-    	{
-    		//render("Empty!!!");
-    		render(body);
-    		// add error message
-    		return;
-    	}
-    	e.sessionId = Session.current().getId();
-   
-    	e.Save(); 
-    } 
-  
-    public static void handleJson(JsonObject body) 
-    {        
-    	renderText(body.getAsJsonPrimitive("name").getAsString());    
-    }        
-    
-    public static ClickStreamEvent handleJsonAsObject(JsonObject body) 
-    {        
-    	ClickStreamEvent e = new Gson().fromJson(body, ClickStreamEvent.class);        
-    	return e; 
-    }
-    
-    public static void Find(JsonObject obj)
-    {
-    
-    
-    	try {
-			DataFinder finder = new DataFinder(ClickStreamEvent.class).Where(obj.get("field").toString(), ConditionTypes.Equals, obj.get("fieldValue").toString());
-		} catch (NoSuchFieldException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    
-    }
+
     
 
 }
