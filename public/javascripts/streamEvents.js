fb9c96c00857a8732153a9b7db0237f801c3eeb7
Anton Zhavoronkov <djzhan@list.ru>
2011-12-04 11:36:56 +0800

    streamEvents js refactor

--- public/javascripts/streamEvents.js
+++ public/javascripts/streamEvents.js
@@ -1,29 +1,32 @@
 $(document).ready(function () {
 	
-
+	initFilter();
 	
-	//$.datepicker.setDefaults({ dateFormat: 'yy-mm-dd' });
-	$("#datepicker1").datetimepicker({dateFormat: 'yy-mm-dd'});
-	$("#datepicker2").datetimepicker({dateFormat: 'yy-mm-dd'});
-	
-	
-	requestData({});
+	requestData(null);
 	
 	$("#apply_filter").click(function () {
-		var filter = {"elementType": $('#element_type').val(), "elementId": $('#element_id').val(), "elementClass": $('#element_class').val(),
-				"created_at_start":$('#datepicker1').val(),
-				"created_at_finish":$('#datepicker2').val()};
-		
-		requestData(filter);
-		
+		requestData(getFilterObject());	
 	});
 	
 });
+
+initFilter = function () {
+	$("#datepicker1").datetimepicker({dateFormat: 'yy-mm-dd'});
+	$("#datepicker2").datetimepicker({dateFormat: 'yy-mm-dd'});
+};
 
 requestData = function (filter) {
 	$.post("streamevents/find", JSON.stringify(filter),function (data) {
 		iterateData(data);
 	}, "json");
+};
+
+getFilterObject = function () {
+	return {"elementType": $('#element_type').val(), 
+		    "elementId": $('#element_id').val(), 
+		    "elementClass": $('#element_class').val(),
+		    "created_at_start":$('#datepicker1').val(),
+		    "created_at_finish":$('#datepicker2').val()};
 };
 
 
